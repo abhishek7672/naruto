f9887d058029d83cb2bfabd15e8d0adad8b31c2e
abhishek7672 <35527762+abhishek7672@users.noreply.github.com>
2018-04-26 06:02:32 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
+,#include <iostream>
  
 using namespace std;
  
 int main()
 {
-   int a, b, c;
+   int a, b, c,d;
  
    cout << "Enter two integers to subtract";
    cin >> a >> b;
  
    c = a -b;
    cout <<"subtaction of the numbers: " << c << endl;
+ cout<<"enter the two no for addition";
+ d=a+b;
+ cin>>a>>b;
+ cout<<d;
  
    return 0;
 }
